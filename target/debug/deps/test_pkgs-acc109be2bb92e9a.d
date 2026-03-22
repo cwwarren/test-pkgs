a5976b3bd45fe81514b5cbd3cb08d2845fb82df8
Colin Warren <git@cwarren.org>
2026-03-22 12:04:52 -0700

    Explicit package discovery for setuptools

---
+++ target/debug/deps/test_pkgs-acc109be2bb92e9a.d
@@ -0,0 +1,5 @@
+/private/tmp/test-pkgs/target/debug/deps/test_pkgs-acc109be2bb92e9a.d: src/lib.rs
+
+/private/tmp/test-pkgs/target/debug/deps/libtest_pkgs-acc109be2bb92e9a.rmeta: src/lib.rs
+
+src/lib.rs:
